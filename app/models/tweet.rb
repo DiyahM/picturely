14b6875ec94ea727e0efb3d2eeaa2746e8779699
Bosco So <rubymeetup@boscoso.com>
2011-07-15 23:14:56 +0800

    simplified creation of Tweeter

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,14 +3,13 @@ class Tweet < ActiveRecord::Base
 
   def self.create_or_update(identifier, text, created_at,
                             from_user_identifier, from_user,
-                            profile_image_url, source, geo,
+                            profile_img, source, geo,
                             provider_name, oembed_type, url)
-    user = Tweeter.find_or_create_by_identifier from_user_identifier
-    user.username = from_user
-    user.profile_image_url = profile_image_url
-    user.save!
+    u = Tweeter.find_or_create_by_identifier(from_user_identifier,
+                                             :username => from_user,
+                                             :profile_image_url => profile_img)
 
-    t = user.tweets.find_or_create_by_identifier identifier
+    t = u.tweets.find_or_create_by_identifier(identifier)
     t.tweet_text = text
     t.tweet_created_at = created_at
     t.source = source
