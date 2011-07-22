d55e5384dc3a18cec9383c874a612be2dfdbf440
Hadiyah Mujhid <diyahm@aol.com>
2011-07-22 06:53:49 -0700

    rm unwanted mig

--- db/migrate/20110722125544_add_album_id_to_uploads.rb
+++
@@ -1,9 +0,0 @@
-class AddAlbumIdToUploads < ActiveRecord::Migration
-  def self.up
-    add_column :uploads, :album_id, :integer
-  end
-
-  def self.down
-    remove_column :uploads, :album_id
-  end
-end
