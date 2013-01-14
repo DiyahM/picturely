eaf533643019c0d08818e3eba61419f99fa4f411
Diyah <diyahm@aol.com>
2013-01-14 11:04:59 -0800

    Add heroku env config

---
+++ config.js
@@ -0,0 +1,7 @@
+ntwitter = require('ntwitter')
+exports.twitter = new ntwitter({
+  consumer_key: process.env.TWITTER_KEY,
+  consumer_secret: process.env.TWITTER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_SECRET
+});
